886f6ddf438368ddd74e410918f1c9a93823c50f
jjadav <jjadav@mamkschools.org>
2018-09-13 09:42:28 -0400

    Initial commit

--- src/Ch7Arrays/HelloWorld.java
+++ src/Ch7Arrays/HelloWorld.java
@@ -3,5 +3,6 @@ package Ch7Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Did this change?");
     }
 }
