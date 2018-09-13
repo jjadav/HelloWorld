9c03e7270fe5f5dc48089141a44f1d05679e51a3
jjadav <jjadav@mamkschools.org>
2018-09-13 09:49:15 -0400

    another line added, helloworld

--- src/Ch7Arrays/HelloWorld.java
+++ src/Ch7Arrays/HelloWorld.java
@@ -3,6 +3,7 @@ package Ch7Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Did this change?");
+        System.out.println("Hello world2!");
+
     }
 }
