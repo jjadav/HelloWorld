29879e1f503dce26737dc8ec715a35ab3d940b11
jjadav <jjadav@mamkschools.org>
2018-09-13 10:10:20 -0400

    helloworld line 2 deleted

--- src/Ch7Arrays/HelloWorld.java
+++ src/Ch7Arrays/HelloWorld.java
@@ -3,7 +3,6 @@ package Ch7Arrays;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world2!");
 
     }
 }
