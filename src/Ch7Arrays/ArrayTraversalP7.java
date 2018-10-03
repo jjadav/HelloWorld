8b45fa1df4502eef9f81fd696fefe3a9bbdac124
jjadav <jjadav@mamkschools.org>
2018-10-03 13:04:57 -0400

    added two algorithms to reverse an array

--- src/Ch7Arrays/ArrayTraversalP7.java
+++ src/Ch7Arrays/ArrayTraversalP7.java
@@ -1,19 +1,40 @@
 package Ch7Arrays;
 
+import java.util.Arrays;
+
 public class ArrayTraversalP7 {
     public static void main(String[] args){
-        int[] array = {5, 2, 4, 1, 3};
-        oliverAlgo(array);
+        int[] array = {5, 2, 4, 1};
+        System.out.println("before calling Michelle & Matthew's algo " +
+                Arrays.toString(array));
+        michelleMattAlgo(array);
+        System.out.println("after calling Michelle & Matthew's algo " +
+                Arrays.toString(array));
     }
 
     //Oliver algorithm reverses the array by using a temporary
     //array
-    public static void oliverAlgo(int[] oa) {
-
+    public static int[] oliverAlgo(int[] oa) {
+        int[] reversed = new int[oa.length];
+        for(int i = reversed.length-1; i>=0; i--){
+            reversed[i] = oa[oa.length-i-1];
+        }
+        oa = reversed;
+        return oa;
+        //System.out.println(Arrays.toString(reversed));
     }
 
     //Michelle & Matthew's algorithms suggests that we do an
     //in-array swap of first and last elements and continue to do
     //so until length of the array divide by 2
+
+    public static void michelleMattAlgo(int[] mma) {
+        int temp;
+        for (int i = 0; i < mma.length / 2; i++) {
+            temp = mma[i];
+            mma[i] = mma[mma.length - i - 1];
+            mma[mma.length - i - 1] = temp;
+        }
+    }
 
 }
